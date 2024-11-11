bf3cd7fbe39e144a4825a172b1734c6cded11d96
daniil <dan.kim.04lb@gmail.com>
2024-11-11 18:15:30 +0500

    feat: replace danate url

--- src/components/DonateButton.svelte
+++ src/components/DonateButton.svelte
@@ -4,14 +4,14 @@
   import { HeartIcon } from 'components/icons'
 
   function _openDonationWindow() {
-    openExternalLink('https://pkoin.net/daniel')
+    openExternalLink('https://www.vlast.xyz/cryptodonation')
   }
 </script>
 
 <button
   on:click={_openDonationWindow}
   rel="noopener noreferrer"
-  class="btn btn-bright btn-full flex items-center justify-center gap-2 text-center text-center"
+  class="btn btn-bright btn-full flex items-center justify-center gap-2 text-center"
 >
   <HeartIcon />
   Поддержать криптовалютой
